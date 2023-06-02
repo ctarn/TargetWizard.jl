57a74efc66f19c1008837f50a193d67cf88743c3
Tarn Yeong Ching <i@ctarn.io>
2023-06-02 18:45:56 +0800

    include `XLCoverageReport`

--- src/TargetWizard.jl
+++ src/TargetWizard.jl
@@ -3,12 +3,14 @@ module TargetWizard
 include("TargetSelect.jl")
 include("report/BasicAquisitionReport.jl")
 include("report/TargetSelectionReport.jl")
+include("report/XLCoverageReport.jl")
 include("view/TargetXView.jl")
 include("view/TargetXDualView.jl")
 
 main_TargetSelect()::Cint = TargetSelect.julia_main()
 main_BasicAquisitionReport()::Cint = BasicAquisitionReport.julia_main()
 main_TargetSelectionReport()::Cint = TargetSelectionReport.julia_main()
+main_XLCoverageReport()::Cint = XLCoverageReport.julia_main()
 main_TargetXView()::Cint = TargetXView.julia_main()
 main_TargetXDualView()::Cint = TargetXDualView.julia_main()
 
