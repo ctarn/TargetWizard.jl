f2da2b155830e9c2201894c0f32cbbbc0cc4f2e7
Tarn Yeong Ching <i@ctarn.io>
2023-10-09 13:01:23 +0800

    util: adjust compile env

--- util/compile.jl
+++ util/compile.jl
@@ -1,22 +1,17 @@
 import PackageCompiler
-import Pkg
 import TOML
 
-rm("Manifest.toml", force=true)
+exes = [
+    "TargetSelect",
+    "BasicAquisitionReport", "TargetSelectionReport", "XLCoverageReport",
+    "TargetXView", "TargetXDualView", "XSiteView", "XExhaustiveSearchView",
+]
 
 cfg = TOML.parsefile("Project.toml")
-
 dir = "tmp/$(Sys.ARCH).$(Sys.iswindows() ? "Windows" : Sys.KERNEL)/$(cfg["name"])"
 
-exes = ["TargetSelect", "BasicAquisitionReport", "TargetSelectionReport", "XLCoverageReport", "TargetXView", "TargetXDualView", "XSiteView", "XExhaustiveSearchView"]
-deps = ["../MesMS.jl", "../MesUtil.jl"]
-Pkg.develop([Pkg.PackageSpec(path=dep) for dep in deps])
-Pkg.resolve()
-PackageCompiler.create_app(".", dir;
-    executables=[exe => "main_$(exe)" for exe in exes],
+PackageCompiler.create_app(".", dir; executables=[exe => "main_$(exe)" for exe in exes],
     force=true, include_lazy_artifacts=true, include_transitive_dependencies=true,
 )
 
-open(joinpath(dir, "VERSION"); write=true) do io
-    write(io, cfg["version"])
-end
+open(io -> write(io, cfg["version"]), joinpath(dir, "VERSION"); write=true)
