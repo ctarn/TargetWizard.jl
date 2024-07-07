9441747b789a660b7fa8f7f35c016ffe462663e1
Tarn Yeong Ching <i@ctarn.io>
2024-07-07 19:59:45 +0800

    doc: adjust build script

--- doc/build.jl
+++ doc/build.jl
@@ -3,30 +3,25 @@ import Documenter
 import TargetWizard
 
 repo = "github.com/ctarn/TargetWizard.jl.git"
-
 root = "doc"
 tmp = "tmp"
-out = joinpath(root, tmp) |> mkpath
 dst = "doc"
 
-vs = readdir(joinpath(root, "log")) .|> splitext .|> first .|> VersionNumber
-sort!(vs; rev=true)
-logs = map(vs) do v in
-    "<section>$(read(joinpath(root, "log", "$(v).html"), String))</section>"
-end
+out = joinpath(root, tmp) |> mkpath
 html = read(joinpath(root, "index.html"), String)
-html = replace(html, "{{ release }}" => "<div class=\"release\">$(join(logs))</div>")
+logs = map((VersionNumber∘first∘splitext).(readdir(joinpath(root, "log"))) |> sort) do v
+    "<section>$(read(joinpath(root, "log", "$(v).html"), String))</section>"
+end |> reverse |> join
+html = replace(html, "{{ release }}" => "<div class=\"release\">$(logs)</div>")
 open(io -> write(io, html), joinpath(out, "index.html"); write=true)
-
 Documenter.deploydocs(; repo, target=tmp, versions=nothing, cname="targetwizard.ctarn.io")
 
 pages = [
     "index.md",
     "guide.md",
-    "Tutorial" => ["tutorial/prot.md", "tutorial/xl.md"],
-    "Manual" => ["manual/index.md", "manual/report.md", "manual/view.md"],
+    "Tutorial" => joinpath.("tutorial", ["prot.md", "xl.md"]),
+    "Manual" => joinpath.("manual", ["index.md", "report.md", "view.md"]),
     "dev.md",
 ]
-
 Documenter.makedocs(; sitename="TargetWizard", build=joinpath(tmp, dst), pages)
 Documenter.deploydocs(; repo, target=joinpath(tmp, dst), dirname=dst)
